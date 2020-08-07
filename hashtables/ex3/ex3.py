a4c1f62a472d2328c7f1d8b0520738af4899fcda
Alex Martinez <alexmartinez0071234@gmail.com>
2020-08-07 12:01:55 -0400

    ex3 works with 1 error

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,11 +1,20 @@
+count = {}
+matches = []
 def intersection(arrays):
     """
     YOUR CODE HERE
     """
     # Your code here
-
-    return result
-
+    for lsts in arrays:
+        for values in lsts:
+            if values not in count:
+                count[values] = 1
+            else:
+                count[values] += 1
+        for value in count:
+            if count[value] == len(arrays):
+                matches.append(value)
+    return matches
 
 if __name__ == "__main__":
     arrays = []
